0eb8aa82d3df840ab630c3f9611855dc096f2c6b
WalterCosta <156-201707140059@users.noreply.gitlab.lasse.ufpa.br>
2022-01-16 21:56:10 -0300

    Teste requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet!</div>;
+    const { emailField } = this.props;
+    return (
+      <div>
+        <header>
+          <p data-testid="email-field">
+            {emailField}
+          </p>
+          <p data-testid="total-field">
+            0
+          </p>
+          <p data-testid="header-currency-field">
+            BRL
+          </p>
+        </header>
+        TrybeWallet!
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  emailField: state.user.email,
+});
+
+Wallet.propTypes = {
+  emailField: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
